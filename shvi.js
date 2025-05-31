81a17f23f0a52c6e4fd4c5dd882e99d8a8037fc8
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2025-05-31 17:17:05 +0400

    Added audio streaming

--- shvi.js
+++ shvi.js
@@ -27,6 +27,59 @@ const processFile = async (filePath, outputPath) => {
   }
 };
 
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const continuousPCMStream = async (filePath) => {
+  const fileContent = await Deno.readTextFile(filePath);
+  const syntaxTree = tokenize(fileContent);
+  const pcm = {
+    pcmArray: [],
+    pcmPtr: 0,
+  };
+  evaluate(syntaxTree, globalSymbolTable, pcm);
+
+  const numChannels = 2;
+
+  const bitsPerSample = globals.WAV_BITS;
+  const bytesPerSample = globals.WAV_BITS / 8;
+
+  const buffer = new ArrayBuffer(
+    pcm.pcmArray.length * numChannels * bytesPerSample,
+  );
+
+  const view = new DataView(buffer);
+  const samples = pcm.pcmArray;
+
+  for (let i = 0; i < samples.length; i++) {
+    const sample = samples[i];
+    const offset = i * numChannels * bytesPerSample;
+
+    for (let ch = 0; ch < numChannels; ch++) {
+      const chOffset = offset + ch * bytesPerSample;
+
+      const clamped = Math.max(
+        globals.SAMPLE_MIN,
+        Math.min(globals.SAMPLE_MAX, sample),
+      );
+
+      if (bitsPerSample === 16) {
+        view.setInt16(chOffset, clamped, true);
+      } else if (bitsPerSample === 32) {
+        view.setInt32(chOffset, clamped, true);
+      } else {
+        throw new Error(`Shvi: Unsupported WAV bit depth: ${bitsPerSample}`);
+      }
+    }
+  }
+
+  const bytesWritten = await Deno.stdout.write(
+    new Uint8Array(buffer),
+  );
+
+  await delay(bytesWritten / globals.SAMPLE_RATE);
+  await continuousPCMStream(filePath);
+};
+
 const runREPL = () => {
   console.log(`🪈 Shvi ver. ${globals.SHVI_VERSION}`);
   console.log(
@@ -85,8 +138,7 @@ const main = async () => {
 
   if (filePath) {
     if (stream) {
-      console.log("Streaming", filePath);
-      console.error("Not Implemented");
+      continuousPCMStream(filePath);
     } else {
       try {
         await processFile(filePath, outputPath);
